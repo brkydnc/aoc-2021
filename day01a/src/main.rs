92080c7714f67d8eb025298e84316f1b83f617d7
Berkay Dinç <berkayydinc@gmail.com>
2021-12-09 18:43:09 +0300

    Solve day01 a & b

---
+++ day01a/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let numbers = include_str!("../input.txt")
+        .lines()
+        .map(|l| l.parse::<i32>().unwrap());
+
+    let count = numbers
+        .clone()
+        .zip(numbers.skip(1))
+        .fold(0, |total, (a, b)| if b > a { total + 1 } else { total });
+
+        println!("{:?}", count);
+}
